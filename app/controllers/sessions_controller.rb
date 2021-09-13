8a43b834a797494c9ce2bb605bc7dfe6e1251c5c
Wendy Harris <wendyv510@hotmail.com>
2021-09-13 15:00:30 -0400

    logout and sessions crud

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
     def welcome
     end 
 
-    
-    
+    def new 
+    end 
+
+    def create
+    end 
+
+    def destroy
+    end 
+   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root 'sessions#welcome' 
   get '/signup' => 'users#new' 
   get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create' 
+  post '/login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'  
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
