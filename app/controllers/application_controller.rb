64aa0e4c264c23d95b657e5a6231052f75605943
Wendy Harris <wendyv510@hotmail.com>
2021-09-14 13:00:40 -0400

    app contr helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,20 @@
 class ApplicationController < ActionController::Base
 
-    private
+
+    helper_method :current_user 
+    helper_method :logged_in?
+    helper_method :authentication_required 
+    
+        def authentication_required
+            if !logged_in 
+                redirect_to login_path 
+            end 
+        end 
+
+        def logged_in! 
+            !!current_user 
+        end 
+
         def current_user 
             @current_user ||= User.find_by_id(session[:user_id]) 
         end 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params) 
             if @user.save 
-                session[:user_id] = @user.id 
-                redirect_to users_path(@user)
+                redirect_to login_path 
             else 
                 render :new 
             end 
