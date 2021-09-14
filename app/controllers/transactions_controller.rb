c680f640b62de90b308db91c419fecac1e6650c1
Wendy Harris <wendyv510@hotmail.com>
2021-09-14 12:04:47 -0400

    transaction routes

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,29 @@
 class TransactionsController < ApplicationController
+
+    def index 
+        @transactions = Transaction.all 
+    end 
+
+    def new
+        @transaction = Transaction.new 
+    end 
+
+    def create
+        @transaction = Transaction.new(transaction_params)
+        current_user.transactions.build 
+            if @transaction.save 
+                redirect_to user_path(@user) 
+            else 
+                render :new 
+            end 
+    end 
+
+    private 
+
+    def transaction_params
+        params.require(:transaction).permit(:payer, :points, :date, :user_id)
+    end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  resources :transactions
+  
   root 'sessions#welcome' 
   get '/signup' => 'users#new' 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'  
   resources :users
+
+  resources :transactions
+  get '/all_payers_points' => 'transactions#index' 
+  get '/add_transaction' => 'transactions#create' 
+  get '/spend_points' => 'transactions#index' 
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
